4f50680bee0697bfd70ef5927fe1ca1b4600dfd5
Nguyen Tan Tu <Administrator@tantu.ringier.com.vn>
2016-09-23 09:38:53 +0700

    no message

---
+++ BeautyAndCare/BeautyAndCare/Controllers/NavbarController.cs
@@ -0,0 +1,20 @@
+﻿using BeautyAndCare.Domain;
+using BeautyAndCare.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeautyAndCare.Controllers
+{
+    public class NavbarController : Controller
+    {
+        // GET: Navbar
+        public ActionResult Index()
+        {
+            var data = new Data();
+            return PartialView("_Navbar", data.navbarItems().ToList());
+        }
+    }
+}
