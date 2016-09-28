d2128c01e7b1ec6638d5d7f7cb99d514cbbb4672
nguyen tan tu <tunguyen.it.dev@gmail.com>
2016-09-29 00:11:27 +0700

    no message

---
+++ BeautyAndCare/BeautyAndCare/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeautyAndCare.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Header()
+        {
+            return View();
+        }
+        public ActionResult Footer()
+        {
+            return View();
+        }
+        public ActionResult Slider()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BeautyAndCare/BeautyAndCare/js/cusInWeb.js
@@ -0,0 +1,49 @@
+﻿
+$(document).ready(function () {
+    $("#menu-icon").on("click", function () {
+        $("#menu-gadget .menu").slideToggle();
+        $(this).toggleClass("active");
+    });
+
+    $('#menu-gadget .menu').find('li>ul').before('<i class="fa fa-angle-down"></i>');
+    $('#menu-gadget .menu li i').on("click", function () {
+        if ($(this).hasClass('fa-angle-up')) {
+            $(this).removeClass('fa-angle-up').parent('li').find('> ul').slideToggle();
+        }
+        else {
+            $(this).addClass('fa-angle-up').parent('li').find('> ul').slideToggle();
+        }
+    });
+});
+
+
+
+var num = 80; //number of pixels before modifying styles
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > num) {
+        $('.isStuck').css('top', '-48px');
+        $('.isStuck').css('visibility', 'hidden');
+        $('.isStuck').css('width', '100%');
+        $('.isStuck').css('margin-top', 'hidden');
+        $('.isStuck').css('visibility', 'hidden');
+    } else {
+        $('#main-header1').removeClass('fixed');
+    }
+});
+
+
+$(document).ready(function () {
+    $('body').data('home_url', '');
+    //$('body').data('page_url', '');
+    $('body').data('extension', '.html');
+    Product.init();
+    WebCommon.init();
+    // alert("-Golbal aler- "+$('body').data('home_url'));
+});
+
+
+$(".lentop").click(function (event) {
+    $('html, body').animate({ scrollTop: 0 }, 1000);
+});
+
